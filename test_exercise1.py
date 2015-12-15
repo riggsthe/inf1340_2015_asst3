e12547b6289bc647af655a81a753fd60864c7919
duki_miletic <dusan.miletic@utoronto.ca>
2015-12-15 13:58:25 -0500

    Test Cases, change employee structure to students. added exception to project attribute, and empty test case

--- test_exercise1.py
+++ test_exercise1.py
@@ -16,20 +16,23 @@ from exercise1 import selection, projection, cross_product
 # TABLES ##
 ###########
 
-EMPLOYEES = [["Surname", "FirstName", "Age", "Salary"],
-             ["Smith", "Mary", 25, 2000],
-             ["Black", "Lucy", 40, 3000],
-             ["Verdi", "Nico", 36, 4500],
-             ["Smith", "Mark", 40, 3900]]
 
-R1 = [["Employee", "Department"],
-      ["Smith", "sales"],
-      ["Black", "production"],
-      ["White", "production"]]
 
-R2 = [["Department", "Head"],
-      ["production", "Mori"],
-      ["sales", "Brown"]]
+STUDENTS = [["FirstName", "Surname", "IQ", "GPA"],
+             ["Hoban", "Washburne", 94, 3.2],
+             ["Zoe", "Washburne", 110, 3.5],
+             ["Malcolm", "Reynolds", 81, 2.6],
+             ["Jayne", "Cobb", 55, 1.1],
+             ["Inara", "Serra", 158, 4.0]]
+
+R1 = [["Student", "Class"],
+      ["Zoe", "Programming"],
+      ["Jayne", "Web Design"],
+      ["Inara", "Programming"]]
+
+R2 = [["Class", "Professor"],
+      ["Programming", "Sim"],
+      ["Web Design", "Yu"]]
 
 
 #####################
@@ -48,14 +51,14 @@ def is_equal(t1, t2):
 #####################
 def filter_students(row):
     """
-    Check if employee represented by row
+    Check if Student represented by row
     is AT LEAST 30 years old and makes
     MORE THAN 3500.
     :param row: A List in the format:
         [{Surname}, {FirstName}, {Age}, {Salary}]
     :return: True if the row satisfies the condition.
     """
-    return row[-2] >= 30 and row[-1] > 3500
+    return row[-2] >= 90 and row[-1] > 3.4
 
 
 ###################
@@ -68,24 +71,37 @@ def test_selection():
     """
 
     result = [["FirstName", "Surname", "IQ", "GPA"],
-              ["Verdi", "Nico", 36, 4500],
-              ["Smith", "Mark", 40, 3900]]
+              ["Jayne", "Cobb", 55, 1.1],
+              ["Inara", "Serra", 158, 4.0]]
 
     assert is_equal(result, selection(STUDENTS, filter_students))
 
+def test_selection_empty():
+    assert selection(STUDENTS_EMPTY, filter_students) is\
+None
 
-def test_projection():
+
+def test_project():
     """
     Test projection operation.
     """
 
     result = [["FirstName", "Surname"],
-              ["Smith", "Mary"],
-              ["Black", "Lucy"],
-              ["Verdi", "Nico"],
-              ["Smith", "Mark"]]
+              ["Hoban", "Washburne"],
+              ["Zoe", "Washburne"],
+              ["Malcolm", "Reynolds"],
+              ["Jayne", "Cobb"],
+              ["Inara", "Serra"]]
 
     assert is_equal(result, projection(STUDENTS, ["FirstName", "Surname"]))
+
+def test_project_unknown_attribute_exception():
+
+    try:
+        project(STUDENTS, ["Does_Not_Exist"])
+    except UnknownAttributeException:
+        assert True
+
 
 
 def test_cross_product():
